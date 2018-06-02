a00747803cef594a160348c490140fa8c769846c
AndiWawanK <andifirwansyah250@gmail.com>
2018-06-02 22:07:22 +0700

    method insert new users

---
+++ application/config/routes.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$route['api/users']['GET'] 			= 'Users/get_all';
+$route['api/user/(:num)']['GET'] 	= 'Users/get/$1';
+$route['api/register']['POST'] 		= 'Users/register';
+$route['api/user/(:num)']['PUT'] 	= 'Users/update/$1';
+$route['api/user/(:num)']['DELETE'] = 'Users/delete/$1';
+$route['api/login'] 				= 'Users/login';
+
+$route['default_controller'] = 'welcome';
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
