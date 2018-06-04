8a40ac66ed2118971d9145d7c4b2c1b8c0ac3a95
AndiWawanK <andifirwansyah250@gmail.com>
2018-06-04 16:33:07 +0700

    config jwt

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+require_once APPPATH .'libraries/JWT.php';
+use \Firebase\JWT\JWT;
 class Users extends CI_Controller {
 
 	public function __construct(){
